8a0bd4d4bd20fe1b6032bee4a0eac827f9a6caf6
Lokesh445 <lokeshkruger2000@gmail.com>
2023-05-24 13:14:58 +0530

    first commit

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport'
 import {Strategy} from 'passport-local'
 
-//without using jwt Authentication
+//without using jwtBearer token aunthentication
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy){
 
